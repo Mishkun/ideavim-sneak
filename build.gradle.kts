504eb572d37f2beff702da052c8e88f516fb3952
Mikhail Levchenko <mishkun.lev@gmail.com>
2020-11-10 00:10:51 +0300

    Update min IDEA version to support AS

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 
 group = "io.github.mishkun"
-version = "1.0"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
@@ -15,7 +15,7 @@ repositories {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2020.2.2"
+    version = "2020.1"
     setPlugins("IdeaVIM:0.60")
 }
 
@@ -26,7 +26,7 @@ tasks.withType<PublishTask> {
 
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
-        Initial plugin release
+        Downgrade to IDEA 2020.1 to support latest Android Studio Release 
       """)
 }
 
